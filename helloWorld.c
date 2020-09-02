111220fc2785a8858972d5cc0e59410f8e0bad2d
David Ryckman <dryckman2@huskers.unl.edu>
2020-09-02 14:06:50 +0000

    Set up hello world

---
+++ helloWorld.c
@@ -0,0 +1,14 @@
+/**
+ *
+ * By David Ryckman
+ *
+ * Hello World Program for GitHub
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc,char **argv){
+    printf("Hello World!\n");
+ }
